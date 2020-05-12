fc261de2fdedf4b778e8e0c2596f4ece08781426
Ogbara Gerrald Godstar <sengodstar@gmail.com>
2020-05-12 20:04:02 +0100

    Add correct social media handles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet, ActivatedRoute, Router } from '@angular/router';
 import { slideInAnimation } from './animation';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Route } from '@angular/compiler/src/core';
 
 
 @Component({
@@ -13,7 +14,35 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 export class AppComponent {
   title = 'Resume';
 
+  routes = ["", "education", "work", "projects", "skills"]
+
+  constructor(private route: ActivatedRoute, private router: Router) { }
+
   prepareRoute(outlet: RouterOutlet) {
     return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
+  }
+
+  navigateBack() {
+    let page = this.router.url.split('/')[1];
+    console.log(page, "page")
+    let indx = this.routes.indexOf(page);
+    console.log(indx, "indx");
+    if (indx > 0) {
+      this.router.navigate([this.routes[indx - 1]])
+    } else {
+      this.router.navigate([this.routes[this.routes.length - 1]])
+    }
+  }
+
+  navigateForward() {
+    let page = this.router.url.split('/')[1];
+    console.log(page, "page")
+    let indx = this.routes.indexOf(page);
+    console.log(indx, "indx");
+    if (indx < this.routes.length - 1) {
+      this.router.navigate([this.routes[indx + 1]])
+    } else {
+      this.router.navigate([this.routes[0]])
+    }
   }
 }
